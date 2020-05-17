f9806b6b23c7c97797f3f7d71037beb2720813b6
Malcolm Hall <malcolmrainehall@gmail.com>
2020-05-17 10:39:03 -0400

    Fixed test to use 'w+' mode as default 'r' mode does not create the file.

--- tests/test_file.py
+++ tests/test_file.py
@@ -5,7 +5,7 @@ from PyFile import file
 
 class TestFile(unittest.TestCase):
     def test_abs_path(self):
-        test = file.File("test_file.txt")
+        test = file.File("test_file.txt", mode='w+')
         print(test.abs_path)
         self.assertIsInstance(test.abs_path, str)
         self.assertEqual(os.path.exists(test.abs_path), True)
