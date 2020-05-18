d68e4368fb4b15415cdcf842554cd2e9a6c1cfad
Malcolm Hall <malcolmrainehall@gmail.com>
2020-05-17 21:18:28 -0400

    Removed redundant setup code in tests and modified places value in almostEquals assertion

--- tests/test_file.py
+++ tests/test_file.py
@@ -6,7 +6,6 @@ from PyFile import file
 
 
 class TestFile(unittest.TestCase):
-
     def setUp(self) -> None:
         self.test = file.File("test_file.txt", mode='w+')
 
@@ -14,43 +13,31 @@ class TestFile(unittest.TestCase):
         self.test.delete()
 
     def test_ext(self):
-        # test = file.File("test_file.txt", mode='w+')
         self.assertIsInstance(self.test.ext, str)
         self.assertTrue(self.test.ext.startswith('.'))
         self.assertEqual(len(self.test.ext), 4)
-        # test.delete()
 
     def test_mode(self):
-        # test = file.File("test_file.txt", mode='w+')
         self.assertEqual(self.test.mode, 'w+')
         self.assertIsInstance(self.test.mode, str)
-        # test.delete()
 
     def test_owner(self):
-        # test = file.File("test_file.txt", mode='w+')
         self.assertIn(self.test.owner, [os.getlogin(), os.path.expanduser("~")
                                                     .replace('\\', '/')
                                                     .split('/')[-1]])
         self.assertIsInstance(self.test.owner, str)
-        # test.delete()
 
     def test_rel_path(self):
-        # test = file.File("test_file.txt", mode='w+')
         self.assertIsInstance(self.test.rel_path, str)
         self.assertEqual(os.path.exists(self.test.rel_path), True)
-        # test.delete()
 
     def test_abs_path(self):
-        # test = file.File("test_file.txt", mode='w+')
         self.assertIsInstance(self.test.abs_path, str)
         self.assertEqual(os.path.exists(self.test.abs_path), True)
-        # test.delete()
 
     def test_last_modified(self):
-        # test = file.File("test_file.txt", mode='w+')
-        self.assertAlmostEqual(self.test.last_modified, time.time(), places=2)
+        self.assertAlmostEqual(self.test.last_modified, time.time(), places=1)
         self.assertIsInstance(self.test.last_modified, float)
-        # test.delete()
 
     def test_size(self):
         self.assertEqual(self.test.size, 0)
